341c44626a9ab0410a6aaea097eda0cac1989db3
RISHI GOLI <rg0001.ayconnects@gmail.com>
2024-07-24 16:40:17 +0530

    drawer basic work completed

--- src/components/root/Header.js
+++ src/components/root/Header.js
@@ -1,14 +1,14 @@
 import { Grid, Box, Button } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 
-function Header() {
+function Header({ toggleDrawer }) {
     return (
         <>
             {/* main container */}
-            <Grid style={{ height: '100%', width: '100%', backgroundColor: 'pink', padding: '0.5rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center', boxShadow: '0px 1px 5px black' }}>
+            <Grid style={{ height: '100%', width: '100%', backgroundColor: 'pink', padding: '0.5rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                 {/* left side container */}
                 <Grid style={{ display: 'flex', alignItems: 'center' }}>
-                    <Box><Button><MenuIcon /></Button></Box>
+                    <Box><Button onClick={toggleDrawer}><MenuIcon /></Button></Box>
                     <Box><h3>ABC Tailor</h3></Box>
                 </Grid>
                 {/* right side container */}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,63 @@
-import { Grid } from '@mui/material'
+import { Grid, Drawer, Box, List, ListItem, ListItemText } from '@mui/material'
+import { useState } from 'react'
 import Header from '../components/root/Header'
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles((theme)=>({
+
+}))
 
 export default function HomePage() {
+    const [drawerOpen, setDrawerOpen] = useState(false)
+    function toggleDrawer() {
+        setDrawerOpen(!drawerOpen)
+    }
     return (
         <>
             {/* main container */}
             <Grid style={{ height: '100vh', width: '100vw', backgroundColor: '#f2f2f2', display: 'flex', flexDirection: 'column' }}>
                 {/* header container */}
-                <Grid style={{ height: '4.5rem', width: '100%' }}>
-                    <Header />
+                <Grid style={{ height: '4.5rem', width: '100%', zIndex:'1201' }}>
+                    <Header toggleDrawer={toggleDrawer} />
+                </Grid>
+
+                {/* main content */}
+                <Grid style={{ display: 'flex', flex: 1 }}>
+                    {/* drawer */}
+                    <Drawer variant="persistent" anchor="left" open={drawerOpen} 
+                        transitionDuration={300}
+                        sx={{
+                            '& .MuiPaper-root':{
+                                backgroundColor: 'pink',
+                                top: '4.5rem'
+                            }
+                        }}
+                    >
+                        <Box
+                            sx={{ width: 250 }}
+                            role="presentation"
+                            onClick={toggleDrawer}
+                            onKeyDown={toggleDrawer}
+                        >
+                            <List>
+                                <ListItem button>
+                                    <ListItemText primary="Item 1" />
+                                </ListItem>
+                                <ListItem button>
+                                    <ListItemText primary="Item 2" />
+                                </ListItem>
+                            </List>
+                        </Box>
+                    </Drawer>
+
+                    {/* content container */}
+                    <Grid style={{
+                        flex: 1, backgroundColor: 'red', padding: '1rem', marginLeft: drawerOpen ? 250 : 0,
+                        transition: 'margin 0.3s'
+                    }}>
+                        {/* Your content goes here */}
+                        <Grid style={{ height: '100%', width: '100%', backgroundColor: 'white' }}></Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </>
